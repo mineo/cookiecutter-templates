b4bb51e7d23786cab460adac6d3e849afdf3d782
Wieland Hoffmann <themineo@gmail.com>
2015-04-28 14:14:50 +0200

    Use the full name from cookiecutter

--- python-mit/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}.py
+++ python-mit/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env {{ cookiecutter.python_binary }}
 # coding: utf-8
-# Copyright © {{ cookiecutter.year}}, Wieland Hoffmann
+# Copyright © {{ cookiecutter.year}} {{ cookiecutter.full_name }}
 # License: MIT, see LICENSE for details
