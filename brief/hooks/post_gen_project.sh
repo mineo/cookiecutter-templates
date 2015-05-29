fadf56ba7bc8295b09776d4d673cc580bd609544
Wieland Hoffmann <themineo@gmail.com>
2015-05-29 14:04:49 +0200

    add a letter template

---
+++ brief/hooks/post_gen_project.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+# Strip spaces around { and }
+# Those are necessary for things like { {{cookiecutter....}} }
+sed -e 's/{ /{/' -e 's/ }/}/' -i *.tex
+git init
+git add .
+git commit -m "Initial commit"
