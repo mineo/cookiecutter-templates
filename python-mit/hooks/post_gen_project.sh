a5b7d6b1af1a6096fccfcebcde599b3f4f670101
Wieland Hoffmann <themineo@gmail.com>
2020-05-23 15:51:08 +0200

    Allow generating a python project without PKGBUILD

--- python-mit/hooks/post_gen_project.sh
+++ python-mit/hooks/post_gen_project.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 source $(which virtualenvwrapper.sh) && mkvirtualenv -p /usr/bin/{{ cookiecutter.python_binary }} -a $(pwd) {{ cookiecutter.repo_name }}
+
+if [[ {{ cookiecutter.with_pkgbuild }} != "yes" ]]; then
+    rm -rf misc
+fi
+
 git init
 git add .
 git rm --cached */version.py
